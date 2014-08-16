92899f6984a828dd4d4c3372212fdddc6d6cff3a
Paul Glass <pnglass@gmail.com>
2014-08-15 22:02:02 -0500

    Can now set start and end times for the loop.
    A "toolbox" is added to allow the user to enter a start and
        a stop time.
    The script accepts (roughly) a '[HH:]MM:SS' format.
    The text inputs should turn red on invalid times and green
        when the times are valid.

--- loop_yt.js
+++ loop_yt.js
@@ -16,6 +16,15 @@
         cued:       5,
     };
 
+    var COLORS = {
+        faded_green: '#88ff88',
+        faded_red: '#ffcccc',
+        white: '#ffffff',
+    };
+
+    /* in milliseconds */
+    var TICK_INTERVAL = 500;
+
     /* Returns an object result, where:
      *      result.player_type is one of "html5", "flash", null
      *      result.player is the player element, if player_type is not null
@@ -34,11 +43,102 @@
         return { player_type: null, player: null };
     }
 
-    function handle_html5(yp) {
-        /* If the video is paused/ended, start it when I click the script */
-        yp.play();
 
-        yp.loop = true;
+    var toolbox_html = '' +
+        '<div id="loop-yt-div" style="position: relative; margin: 5px">' +
+            '<label> Start: <input placeholder="MM:SS" id="loop-yt-start" type="text"/></label>' +
+            '<label> Stop: <input placeholder="MM:SS" id="loop-yt-stop" type="text"/></label>' +
+        '</div>';
+    function get_start_time_input() { return document.getElementById('loop-yt-start') || {}; }
+    function get_stop_time_input() { return document.getElementById('loop-yt-stop') || {}; }
+
+    function create_element_from_html_string(html_string) {
+        var tmp = document.createElement('div');
+        tmp.innerHTML = html_string;
+        return tmp.firstChild;
+    }
+
+    function in_range(val, low, high) { return low <= val && val < high; }
+
+    function clamp(val, min, max) {
+        if (val < min) {
+            return min;
+        } else if (val > max) {
+            return max;
+        }
+        return val;
+    }
+
+    /* Parse [HH:]MM:SS and return the number of seconds. Returns -1 on failure. */
+    function parseSecondsFromString(time_str) {
+        var parts = time_str.trim().split(':');
+        if (parts.length != 2 && parts.length != 3) {
+            return -1;
+        }
+
+        /* pop() returns undefined on empty array.
+         * parseInt returns NaN on invalid input.
+         * Accept strings of length two for seconds.
+         * Accept strings of length one or two for minutes.
+         * Accept strings of any length for hours.
+         */
+        var ss_str = parts.pop();
+        var mm_str = parts.pop();
+        var hh_str = parts.pop();
+        var ss = Number.parseInt(ss_str.length == 2 ? ss_str : "");
+        var mm = Number.parseInt(mm_str.length <= 2 ? mm_str : "");
+        var hh = Number.parseInt(hh_str);
+
+        if ((parts.length == 3 && isNaN(hh))
+            || isNaN(mm) || isNaN(ss)
+            || !in_range(mm, 0, 60) || !in_range(ss, 0, 60)) {
+            return -1;
+        }
+
+        if (!isNaN(hh)) {
+            return 60 * 60 * hh + 60 * mm + ss;
+        } else {
+            return 60 * mm + ss;
+        }
+    }
+
+    function set_background_status_color(element, status) {
+        if (status) {
+            element.style.backgroundColor = COLORS.faded_green;
+        } else {
+            element.style.backgroundColor = COLORS.faded_red;
+        }
+    }
+
+    function get_start_time(max_value) {
+        var n_secs = parseSecondsFromString(get_start_time_input().value || "");
+        set_background_status_color(get_start_time_input(),
+                                    n_secs >= 0 && n_secs < max_value);
+        if (n_secs > max_value) {
+            return 0;
+        } else {
+            return clamp(n_secs, 0, max_value);
+        }
+    }
+
+    function get_stop_time(max_value) {
+        var n_secs = parseSecondsFromString(get_stop_time_input().value || "");
+        var start_time = get_start_time(max_value);
+        set_background_status_color(get_stop_time_input(),
+                                    n_secs > start_time && n_secs <= max_value);
+        return clamp(n_secs, start_time, max_value);
+    }
+
+    function register_time_input_callbacks(video_duration) {
+        get_start_time_input().oninput = get_start_time(video_duration);
+        get_stop_time_input().oninput = get_stop_time(video_duration);
+    }
+
+    function create_toolbox() {
+        var watch_header_div = document.getElementById('watch-header');
+        var loop_yt_div = create_element_from_html_string(toolbox_html);
+        watch_header_div.insertBefore(loop_yt_div, watch_header_div.firstChild);
+        return loop_yt_div;
     }
 
     function video_id_from_url(url) {
@@ -58,6 +158,27 @@
         return subparts[0];
     }
 
+    function handle_html5(yp) {
+        /* If the video is paused/ended, start it when I click the script */
+        register_time_input_callbacks(yp.duration);
+        yp.play();
+
+        yp.loop = true;
+
+        /* use a callback to loop with custom start/end events */
+        function update_func() {
+            var start_time = get_start_time(yp.duration);
+            var stop_time = get_stop_time(yp.duration);
+            if (yp.currentTime < start_time
+                || (start_time < stop_time && stop_time < yp.currentTime)) {
+                yp.currentTime = start_time;
+            }
+        }
+
+        // yp.ontimeupdate = update_func
+        _loop_yt_interval_id = setInterval(update_func, TICK_INTERVAL);
+    }
+
     /* A bug prevents this from working with a callback on state changes
      * in firefox. Instead, just reload the current video within its frame.
      *
@@ -65,6 +186,8 @@
      *     https://code.google.com/p/gdata-issues/issues/detail?id=6531
      */
     function handle_flash(yp) {
+        register_time_input_callbacks(yp.getDuration());
+
         var url = yp.getVideoUrl();
         var video_id = video_id_from_url(url);
         if (video_id === null) {
@@ -78,20 +201,37 @@
          */
         _loop_yt_interval_id = setInterval(function() {
             var state = yp.getPlayerState();
-            if (state === YT_PLAYER_STATES.ended) {
-                /* docs: loadVideoById(id, startSeconds, suggestedQuality)
-                 *        returns nothing.
-                 */
-                yp.loadVideoById(video_id, 0);
+
+            var start_time = get_start_time(yp.getDuration());
+            var stop_time = get_stop_time(yp.getDuration());
+            if (state === YT_PLAYER_STATES.ended
+                || (start_time < stop_time && yp.getCurrentTime() > stop_time - 0.001)
+                || yp.getCurrentTime() < start_time) {
+                /* docs: loadVideoById(id, startSeconds, suggestedQuality), returns nothing. */
+                // yp.loadVideoById(video_id, start_time);
+                yp.seekTo(start_time);
             }
-        }, 1000);
+        }, TICK_INTERVAL);
 
         /* If the video is paused/ended, start it when I click the script */
         var state = yp.getPlayerState();
         if (state === YT_PLAYER_STATES.ended
             || state == YT_PLAYER_STATES.paused
             || state == YT_PLAYER_STATES.unstarted) {
+            var start_time = get_start_time(yp.getDuration());
+            if (yp.getCurrentTime() < start_time) {
+                // yp.loadVideoById(video_id, start_time);
+                yp.seekTo(start_time);
+            }
             yp.playVideo();
+        }
+    }
+
+    function setup_toolbox() {
+        if (document.getElementById('loop-yt-div') === null) {
+            var watch_header = document.getElementById('watch-header');
+            var loop_yt_div = create_element_from_html_string(toolbox_html);
+            watch_header.insertBefore(loop_yt_div, watch_header.firstChild);
         }
     }
 
@@ -109,5 +249,6 @@
     }
 
     console.log("Running LoopYT");
+    setup_toolbox();
     set_loop();
 })();
